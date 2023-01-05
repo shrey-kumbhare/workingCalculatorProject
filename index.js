2a28e730ba60af54e81d756383f81aa8efa0332a
shrey-kumbhare <116801075+shrey-kumbhare@users.noreply.github.com>
2023-01-05 15:17:19 +0530

    Update index.js
    simple javascript working

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// alert("welcome")
+// alert("welcome")alert("welcome")
+
+var first,second,operation;
+var numofbuttons=document.querySelectorAll(".btn").length
+for (var i=0;i< numofbuttons;i++)
+{
+    document.querySelectorAll("button")[i].addEventListener("click",function (){
+          
+        if(this.innerHTML==="C")
+        {
+            document.querySelector("input").value= " ";
+        }
+        else if(this.innerHTML==="=")
+        {
+            second=document.querySelector("input").value;
+            document.querySelector("input").value= " ";
+            compute(parseFloat(first),parseFloat(second),operation)
+        }
+        else if(this.innerHTML==="/" || this.innerHTML==="-"|| this.innerHTML==="+"|| this.innerHTML==="*")
+        {
+            operation=this.innerHTML;
+            first=document.querySelector("input").value;
+            document.querySelector("input").value= " ";
+
+        }
+        else{
+            document.querySelector("input").value+=this.innerHTML;
+
+        }
+    
+    }
+    );
+}
+function compute(f,s,o){
+    var ans;
+    if(o==="/"){
+       ans=f/s;
+       document.querySelector("input").value=ans;
+    }else if (o==="-"){
+        ans=f-s;
+        document.querySelector("input").value=ans;
+     }
+     else if (o==="+"){
+        ans=f+s;
+        document.querySelector("input").value=ans;
+     }
+     else if (o==="*"){
+        ans=f*s;
+        document.querySelector("input").value=ans;
+     }
+
+}
